25aaa17a2b03108c696b516c6447f5ae01e23f61
luiscarlos8228 <129421341+luiscarlos8228@users.noreply.github.com>
2023-08-31 17:06:37 -0300

    Update rpg_inicio.py
    bugs fixed.
    adicionado musica de abertura, encerramento e audio ao responder perguntas

--- rpg_inicio.py
+++ rpg_inicio.py
@@ -1,4 +1,16 @@
 import pickle
+import time
+import os
+import sys
+
+
+with open(os.devnull, 'w') as fnull:
+    old_stdout = sys.stdout
+    sys.stdout = fnull
+    import pygame
+    sys.stdout = old_stdout
+
+pygame.init()
 
 opcoes_validas = ["1", "2", "3"]
 
@@ -21,21 +33,31 @@ dados_personagem = {
     "raca": ""
 }
 
+def audio(som):
+    pygame.mixer.init()
+    sound = pygame.mixer.Sound(som)
+    sound.play()
+    time.sleep(2)
+    pygame.mixer.quit()
+    return
 
 def escolher_raca():
     while True:
         raca_personagem = input(
             "Qual é a raça do seu personagem?\nhumano\nelfo\nanão\nfada\nanimal\nvampiro\ngoblin\ntroll \n R: ").strip().lower()
+        audio("select.wav")
 
         if raca_personagem in racas_descricoes:
             descricao_raca = racas_descricoes[raca_personagem]
             print(descricao_raca)
 
             escolha_raca = input("Você deseja confirmar essa raça ou ver outra? (confirmar/outra): ").strip().lower()
+            audio("select.wav")
 
             while escolha_raca != "confirmar" and escolha_raca != "outra":
                 print("Escolha inválida.")
                 escolha_raca = input("Você deseja confirmar essa raça ou ver outra? (confirmar/outra): ").strip().lower()
+                audio("select.wav")
 
             if escolha_raca == "confirmar":
                 dados_personagem["raca"] = raca_personagem
@@ -43,6 +65,7 @@ def escolher_raca():
                 break
         else:
             print("Raça inválida. Escolha uma das opções disponíveis.")
+            audio("erro.wav")
 
 def salvar_dados(dados, dadinhos):
     try:
@@ -59,13 +82,18 @@ def carregar_dados(dadinhos):
             return pickle.load(arquivo)
     except FileNotFoundError:
         return None
+audio("musics.wav")
 
-
-esc = input("Você quer:\n1 - Começar um novo jogo\n2 - Carregar um jogo salvo\n3 - Sair do programa\nR: ")
+esc = input("Ola! Oque quer fazer? :\n1 - Começar um novo jogo\n2 - Carregar um jogo salvo\n3 - Sair do programa\nR: ")
 
 if esc == "3":
     print("Até a próxima!")
+    audio("musics.wav")
+
+
 elif esc == "2":
+    audio("select.wav")
+    time.sleep(1)
     dados_salvos = carregar_dados('E:/Users/lu/PycharmProjects/pythonProject/projetorpg/dadinhos.pk1')
 
     if dados_salvos:
@@ -76,42 +104,69 @@ elif esc == "2":
 
 elif esc == "1":
     print("Talvez possa ter algum jogo salvo tem certeza de iniciar um novo? ")
-    log = input("sim ou nao ?")
+    audio("select.wav")
+
+    log = input("sim ou nao ? ")
+    audio("select.wav")
+
     if log == "sim":
         print("E lá vamos nós!")
         dados_personagem["nome"] = input("Qual é o nome do seu personagem? ")
+        audio("select.wav")
+
         dados_personagem["idade"] = input("Quantos anos seu personagem tem? ")
+        audio("select.wav")
+
         dados_personagem["altura"] = input("Qual é a altura do seu personagem? ")
+        audio("select.wav")
 
         escolher_raca()
 
-        salvar_dados(dados_personagem, 'E:/Users/lu/PycharmProjects/pythonProject/projetorpg/dadinhos.pk1')  # Salva os dados aqui
+        salvar_dados(dados_personagem, 'E:/Users/lu/PycharmProjects/pythonProject/projetorpg/dadinhos.pk1')
+
     elif log.lower() == "não":
-        print("Encerrando (nome jogo).")
+        audio("encerramento.wav")
+        time.sleep(5)
+        print("Encerrado.")
 
 
+    else:
+        print("Número inválido")
+        audio("erro.wav")
 
-else:
-    print("Número inválido")
-    escolha = input("Quer escolher de novo? (sim/não): ")
+        escolha = input("Quer escolher de novo? (sim/não): ")
+        audio("select.wav")
 
     if escolha.lower() == "sim":
         esc = input("Você quer:\n1 - Começar um novo jogo\n2 - Carregar um jogo salvo\n3 - Sair do programa\nR: ")
         if esc in opcoes_validas:
             if esc == "3":
                 print("Até a próxima!")
+                audio("yamete.wav")
+
             elif esc == "2":
                 print("Ok, vamos carregar um jogo salvo...")
+                audio("select.wav")
+
             elif esc == "1":
                 print("E lá vamos nós!")
+                audio("select.wav")
+
                 nome_personagem = input("Qual é o nome do seu personagem? ")
+                audio("select.wav")
+
                 idade_personagem = input("Quantos anos seu personagem tem? ")
+                audio("select.wav")
+
                 altura_personagem = input("Qual é a altura do seu personagem? ")
+                audio("select.wav")
 
                 escolher_raca()
 
     elif escolha.lower() == "não":
         print("Encerrando (nome jogo).")
+        audio("musics.wav")
 
     else:
         print("Escolha inválida. Por favor, digite 'sim' ou 'não'.")
+        audio("erro.wav")
